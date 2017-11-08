9fe5ea64a52e575f2f85ba7e3b16132c960b1b9e
tpettrov <tpettrov@gmail.com>
2017-11-08 16:54:00 +0200

    logger works

--- src/public/index.php
+++ src/public/index.php
@@ -15,6 +15,16 @@ $config['db']['dbname'] = "exampleapp";
 
 $app = new \Slim\App(['settings' => $config]);
 $container = $app->getContainer();
+
+$container['logger'] = function($c) {
+    $logger = new \Monolog\Logger('my_logger');
+    $file_handler = new \Monolog\Handler\StreamHandler("../logs/app.log");
+    $logger->pushHandler($file_handler);
+    return $logger;
+};
+
+//$container->get('logger')->addInfo("Just checking if Monolog logger actually works");
+
 $app->get('/hello/{name}', function (Request $request, Response $response) {
     $name = $request->getAttribute('name');
     $response->getBody()->write("Hello, $name");
