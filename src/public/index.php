f3075e1e07e95146b0615dfc3058b3fb018c68ac
tpettrov <tpettrov@gmail.com>
2017-11-09 14:39:12 +0200

    logger experimenting

--- src/public/index.php
+++ src/public/index.php
@@ -16,6 +16,8 @@ $config['db']['dbname'] = "exampleapp";
 $app = new \Slim\App(['settings' => $config]);
 $container = $app->getContainer();
 
+//add logger to the dependency injection controller
+
 $container['logger'] = function($c) {
     $logger = new \Monolog\Logger('my_logger');
     $file_handler = new \Monolog\Handler\StreamHandler("../logs/app.log");
@@ -23,12 +25,33 @@ $container['logger'] = function($c) {
     return $logger;
 };
 
+//add db to the dependency injection controller
+
+$container['db'] = function ($c) {
+    $db = $c['settings']['db'];
+    $pdo = new PDO("mysql:host=" . $db['host'] . ";dbname=" . $db['dbname'],
+        $db['user'], $db['pass']);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $pdo;
+};
+
 //$container->get('logger')->addInfo("Just checking if Monolog logger actually works");
+
+
+//adding routes
 
 $app->get('/hello/{name}', function (Request $request, Response $response) {
     $name = $request->getAttribute('name');
     $response->getBody()->write("Hello, $name");
 
     return $response;
-});
+})->setName('hello');
+
+$app->get('/', function (Request $request, Response $response) {
+    $this->logger->addInfo("Home loaded.");
+    $response->getBody()->write("Hello. This is home.");
+    return $response;
+})->setName('home');
+
 $app->run();
